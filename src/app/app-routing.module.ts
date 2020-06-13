17a0b2fd73f2d110e965420d461f7c89f1b7bfd2
alanronin <alanronin@gmail.com>
2020-06-13 14:28:12 -0700

    display userinfo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Person, SearchService } from '../services';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  query: string;
+  searchResults: Array<Person>;
+  sub: Subscription;
+
+  constructor(private searchService: SearchService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      if (params.term) {
+        this.query = decodeURIComponent(params.term);
+        this.search();
+      }
+    });
+  }
+
+  search(): void {
+    this.searchService.search(this.query).subscribe(
+      (data: any) => { this.searchResults = data; },
+      error => console.log(error)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+}
